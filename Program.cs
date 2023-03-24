38162635bebc1bfe53e453e6117b5c2648c8e1d7
Douglas Castro <douglashenriquees@gmail.com>
2023-03-24 06:49:49 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File("logs/myapp.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+Log.Information("Hello, world!");
+
+short x = 10, y = 0;
+
+try
+{
+    Log.Debug("Dividing {A} by {B}", x, y);
+    Console.WriteLine(x / y);
+}
+catch (Exception ex)
+{
+    Log.Error(ex, "Something went wrong");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+* ```dotnet add package Serilog```
+* ```dotnet add package Serilog.Sinks.Console```
+* ```dotnet add package Serilog.Sinks.File```

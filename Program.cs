d12e6446b17f311fd0bd1456916058ac55de8d65
Douglas Castro <douglashenriquees@gmail.com>
2023-04-15 09:35:54 -0300

    add seq dashboard

--- Program.cs
+++ Program.cs
@@ -4,16 +4,17 @@ Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Debug()
     .WriteTo.Console()
     .WriteTo.File("logs/myapp.txt", rollingInterval: RollingInterval.Day)
+    .WriteTo.Seq("http://localhost:5341")
     .CreateLogger();
 
 Log.Information("Hello, world!");
 
-// use {@} to render the objects in json format
+// use {@} to show the objects in json format
 var sensorInput = new { Latitude = 25, Longitude = 134, Point = new { Value = 0.344432002 } };
 Log.Information("Processing {@SensorInput}", sensorInput);
 Log.Information("Procesisng {SensorInput}", sensorInput);
 
-// use {$} to render the types of objects
+// use {$} to show the types of objects
 var unknown = new[] { 1, 2, 3 };
 Log.Information("Received {$Type}", unknown);
 
@@ -21,7 +22,7 @@ short x = 10, y = 0;
 
 try
 {
-    // use {} to render the primitive type values
+    // use {} to show the primitive type values
     Log.Debug("Dividing {A} by {B}", x, y);
     Console.WriteLine(x / y);
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 * ```dotnet add package Serilog```
 * ```dotnet add package Serilog.Sinks.Console```
 * ```dotnet add package Serilog.Sinks.File```
+* ```dotnet add package Serilog.Sinks.Seq```
+
+* ```docker run --name seq --restart unless-stopped -e ACCEPT_EULA=Y -p 80:80 -p 5341:5341 datalust/seq```

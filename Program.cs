b400983f2f18c19ea1e35cfad0b7953e2f3e9a81
Douglas Castro <douglashenriquees@gmail.com>
2023-04-21 11:31:32 -0300

    implementing log context

--- Program.cs
+++ Program.cs
@@ -1,30 +1,36 @@
 ﻿using Serilog;
+using Serilog.Context;
 
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Debug()
     .WriteTo.Console()
     .WriteTo.File("logs/myapp.txt", rollingInterval: RollingInterval.Day)
     .WriteTo.Seq("http://localhost:5341")
+    .Enrich.FromLogContext()
     .CreateLogger();
 
-Log.Information("Hello, world!");
-
-// use {@} to show the objects in json format
-var sensorInput = new { Latitude = 25, Longitude = 134, Point = new { Value = 0.344432002 } };
-Log.Information("Processing {@SensorInput}", sensorInput);
-Log.Information("Procesisng {SensorInput}", sensorInput);
-
-// use {$} to show the types of objects
-var unknown = new[] { 1, 2, 3 };
-Log.Information("Received {$Type}", unknown);
-
-short x = 10, y = 0;
+// use "LogContext.PushProperty" for add extra values in context of the logs
+using var ctx = LogContext.PushProperty("SampleProp", "sample property for context");
 
 try
 {
+    Log.Information("Hello, world!");
+
+    // use {@} to show the objects in json format
+    var sensorInput = new { Latitude = 25, Longitude = 134, Point = new { Value = 0.344432002 } };
+    Log.Information("Processing {@SensorInput}", sensorInput);
+    Log.Information("Procesisng {SensorInput}", sensorInput);
+
+    // use {$} to show the types of objects
+    var unknown = new[] { 1, 2, 3 };
+    Log.Information("Received {$Type}", unknown);
+
+    short x = 10, y = 0;
+
     // use {} to show the primitive type values
     Log.Debug("Dividing {A} by {B}", x, y);
-    Console.WriteLine(x / y);
+
+    var z = x / y;
 }
 catch (Exception ex)
 {
